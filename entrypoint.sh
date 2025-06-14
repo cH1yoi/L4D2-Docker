5bc683bce723a492b6989cc0e85466ba3d3d7c98
cH1yoi <chiyoo1@outlook.com>
2025-06-14 16:51:58 +0800

    try

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-DEFAULT_ARGS="-game left4dead2 -sv_lan 0 host+port 20721 -maxplayers 31 +sv_setmax 31 +exec server.cfg +map c2m1_highway"
-FINAL_ARGS="$DEFAULT_ARGS $*"
+mkdir -p /home/hana/l4d2/left4dead2
+mkdir -p /home/hana/l4d2/left4dead2/addons
+mkdir -p /home/hana/l4d2/left4dead2/maps
 
-if [ -d "/plugins" ] && [ ! -z "$(ls -A /plugins)" ]; then
+if [ -d "/plugins" ] && [ "$(ls -A /plugins)" ]; then
     echo "Creating plugins symlinks..."
     for plugin in /plugins/*; do
-        if [ -f "$plugin" ]; then
-            ln -sf "$plugin" "/home/hana/l4d2/left4dead2/$(basename $plugin)"
-            echo "Linked plugin: $(basename $plugin)"
-        fi
+        [ -e "$plugin" ] && ln -sf "$plugin" "/home/hana/l4d2/left4dead2/addons/$(basename "$plugin")"
+        echo "Linked plugin: $plugin -> /home/hana/l4d2/left4dead2/addons/$(basename "$plugin")"
     done
 fi
 
-if [ -d "/maps" ] && [ ! -z "$(ls -A /maps)" ]; then
+if [ -d "/maps" ] && [ "$(ls -A /maps)" ]; then
     echo "Creating maps symlinks..."
     for map in /maps/*; do
-        if [ -f "$map" ]; then
-            ln -sf "$map" "/home/hana/l4d2/left4dead2/addons/$(basename $map)"
-            echo "Linked map: $(basename $map)"
-        fi
+        [ -e "$map" ] && ln -sf "$map" "/home/hana/l4d2/left4dead2/maps/$(basename "$map")"
+        echo "Linked map: $map -> /home/hana/l4d2/left4dead2/maps/$(basename "$map")"
     done
 fi
 
+echo "Current plugins in addons:"
+ls -l /home/hana/l4d2/left4dead2/addons
+
+echo "Current maps in maps:"
+ls -l /home/hana/l4d2/left4dead2/maps
+
 echo "Starting server with command:"
-echo "/home/hana/l4d2/srcds_run $FINAL_ARGS"
-exec /home/hana/l4d2/srcds_run $FINAL_ARGS
+echo "/home/hana/l4d2/srcds_run $@"
+exec /home/hana/l4d2/srcds_run "$@"
